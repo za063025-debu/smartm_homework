9c6be95162716a060a5f3c20f6340f1f1efba36d
za063025-debu <za063025@gmail.com>
2026-02-19 07:04:34 +0300

    Create main.dart

---
+++ homework1/main.dart
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<head>
+  <meta charset="UTF-8">
+  <title>السيرة الذاتية - زياد الحربي</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap 5 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+
+<div class="container mt-5">
+  <div class="card shadow p-4">
+
+    <div class="text-center">
+      <img src="img/profile.jpg" class="rounded-circle mb-3" width="140">
+      <h2>زياد الحربي</h2>
+      <p class="text-muted">تخصص برمجة</p>
+    </div>
+
+    <hr>
+
+    <h4>نبذة عني</h4>
+    <p>
+      طالب تخصص برمجة أطمح لأن أكون موظفًا منضبطًا وناجحًا في مجال تقنية المعلومات.
+      أسعى لتطوير مهاراتي البرمجية بشكل مستمر والعمل ضمن فريق باحترافية وتحمل المسؤولية.
+    </p>
+
+    <h4>نقاط القوة والمهارات</h4>
+    <ul>
+      <li>إدارة الوقت بكفاءة</li>
+      <li>فهم الأشياء بسرعة</li>
+      <li>الالتزام والانضباط في العمل</li>
+      <li>العمل ضمن فريق</li>
+      <li>القدرة على التعلم الذاتي</li>
+    </ul>
+
+    <h4>الشهادات</h4>
+    <ul>h
+      <li>دورة من منصة سطر</li>
+    </ul>
+
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
